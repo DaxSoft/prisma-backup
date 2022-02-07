088e3d1fbaddb06db332eba288fe00a69f62895d
vorlefan <dax-soft@live.com>
2022-02-07 12:49:40 -0300

    Delete types.ts

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import { PathRoute } from '@vorlefan/path';
-
-export interface PrismaExportJsonProps {
-    models: Record<any, () => Function>;
-    enums: Record<any, () => Record<any, any>>;
-    onRoute?: (route: PathRoute) => any;
-    folderName?: string;
-}
