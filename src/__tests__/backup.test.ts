3b027dc7a425d52cd8b44d803c4754fa4a5ce160
Michael Willian Santos <dax-soft@live.com>
2025-08-29 14:05:35 -0300

    s

--- src/__tests__/backup.test.ts
+++ src/__tests__/backup.test.ts
@@ -12,7 +12,8 @@ describe('PrismaBackup', () => {
     const backup = new PrismaBackup(prisma, { folderName: TMP_BACKUP_FOLDER, database: 'postgres', isTesting: true });
     await backup.run();
 
-    const folders = Route.folders(TMP_BACKUP_FOLDER);
-    expect(folders.length > 0).toBeTruthy();
+    const files = await Route.files(TMP_BACKUP_FOLDER);
+    expect(files.length > 0).toBeTruthy();
+    expect(!!files?.find((d) => d.name === '_prisma_migrations')).toBeTruthy();
   });
 });
