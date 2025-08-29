e3b438ffe84dec71312ec5d38171d9c06391627d
Michael Willian Santos <dax-soft@live.com>
2025-08-29 13:39:21 -0300

    d

---
+++ jest.config.mjs
@@ -0,0 +1,26 @@
+// This is ES Module syntax
+export default {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/__tests__/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  testEnvironment: 'node',
+  preset: 'ts-jest/presets/default-esm',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
